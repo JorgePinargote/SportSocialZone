d29d91574dd08e71594baeef888c7a7367aad43f
ceva94 <ceva94@hotmail.com>
2018-09-01 18:01:38 -0500

    mmm

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        ConsoleTVs\Charts\ChartsServiceProvider::class,
 
     ],
 
@@ -212,8 +211,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Charts' => ConsoleTVs\Charts\Facades\Charts::class,
+
 
     ],
 
 ];
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,98 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <title>Laravel</title>
-
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"/>
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+                    html, body {
+                        background-color: #fff;
+                        color: #636b6f;
+                        font-family: 'Raleway', sans-serif;
+                        font-weight: 100;
+                        height: 100vh;
+                        margin: 0;
+                    }
+        
+                    .full-height {
+                        height: 100vh;
+                    }
+        
+                    .flex-center {
+                        align-items: center;
+                        display: flex;
+                        justify-content: center;
+                    }
+        
+                    .position-ref {
+                        position: relative;
+                    }
+        
+                    .top-right {
+                        position: absolute;
+                        right: 10px;
+                        top: 18px;
+                    }
+        
+                    .content {
+                        text-align: center;
+                    }
+        
+                    .title {
+                        font-size: 84px;
+                    }
+        
+                    .links > a {
+                        color: #636b6f;
+                        padding: 0 25px;
+                        font-size: 12px;
+                        font-weight: 600;
+                        letter-spacing: .1rem;
+                        text-decoration: none;
+                        text-transform: uppercase;
+                    }
+        
+                    .m-b-md {
+                        margin-bottom: 30px;
+                    }
+                </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
+            <div class="top-right links">
+                @auth
+                <a href="{{ url('/home') }}">Home</a>
+                @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+                @endauth
+            </div>
             @endif
-
             <div class="content">
-                <a href="#" id="Usuarios-button">Cargar Usuarios</a> 
+                <a href="#" id="Usuarios-button">Cargar Usuarios</a>
                 <div id="usuarios"></div>
                 <script>
                     $(function(){
-                        $('#Usuarios-button').on('click', funcion(e){
+                        $('#Usuarios-button').on('click',function(e){
                             e.preventDefault();
-                            $('#usuarios-graph').html('cargando...');
-                            $.get('Graficosyreporte.php', function(data){
-                                 var usuarios = '';
-                                 $.each(data, function(){
-                                    usuarios = this + '<br>';
-                                 })
-                                 $('#usuarios-graph').html(usuarios)
-                                 $('#Usuarios-button').hide()
-                            })
-                        })
-                    })
-                </script>   
+                            $('#usuarios').html('cargando...');
+                            $.get('grafico-userstoday',function(data){
+                                var usuarios ='';
+                                $.each(data,function(){
+                                    usuarios=this +'<br>';
+                                });
+                                $('#usuarios').html(usuarios);
+                                $('#Usuarios-button').hide();
+                            });
+                        });
+                    });
+                </script>
             </div>
         </div>
     </body>
