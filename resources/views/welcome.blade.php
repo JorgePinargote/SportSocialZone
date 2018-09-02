d93c51ea6d6a677c6277412efa00b3908f2a85e1
Lenin Lima <lejalima@espol.edu.ec>
2018-09-01 19:13:13 -0500

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="https://d3js.org/d3.v5.min.js"></script>
+        <script src="https://d3js.org/d3.v4.min.js"></script>
         <title>Laravel</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"/>
@@ -64,6 +64,7 @@
                 </style>
     </head>
     <body>
+        <svg width="300" height="200"> </svg>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
             <div class="top-right links">
@@ -85,40 +86,30 @@
                             $('#usuarios').html('cargando...');
                             $.get('grafico-userstoday',function(data){
                                 var usuarios ='';
-                                var vis = d3.select("body")
-                                    .append("svg:svg")              //create the SVG element inside the <body>
-                                    .data([data])                   //associate our data with the document
-                                        .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
-                                        .attr("height", h)
-                                    .append("svg:g")                //make a group to hold our pie chart
-                                        .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
+                                var svg = d3.select("svg"),
+                                    width = svg.attr("width"),
+                                    height = svg.attr("height"),
+                                    radius = Math.min(width, height) / 2,
+                                    g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+                                var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
 
-                                var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
-                                    .outerRadius(r);
+                                var pie = d3.pie();
 
-                                var pie = d3.layout.pie()           //this will create arc data for us given a list of values
-                                    .value(function(d) { return d.value; });    //we must tell it out to access the value of each element in our data array
+                                var arc = d3.arc()
+                                            .innerRadius(0)
+                                            .outerRadius(radius);
 
-                                var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
-                                    .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
-                                    .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
-                                        .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
-                                            .attr("class", "slice");    //allow us to style things in the slices (like text)
+                                var arcs = g.selectAll("arc")
+                                            .data(pie(data))
+                                            .enter()
+                                            .append("g")
+                                            .attr("class", "arc")
 
-                                    arcs.append("svg:path")
-                                            .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
-                                            .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
-
-                                    arcs.append("svg:text")                                     //add a label to each slice
-                                            .attr("transform", function(d) {                    //set the label's origin to the center of the arc
-                                            //we have to make sure to set these before calling arc.centroid
-                                            d.innerRadius = 0;
-                                            d.outerRadius = r;
-                                            return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
-                                        })
-                                        .attr("text-anchor", "middle")                          //center the text on it's origin
-                                        .text(function(d, i) { return data[i].label; });        //get the label from our original data array
-                                    
+                                arcs.append("path")
+                                    .attr("fill", function(d, i) {
+                                        return color(i);
+                                    })
+                                    .attr("d", arc);
                                 $('#usuarios').html(usuarios);
                                 $('#Usuarios-button').hide();
                             });
