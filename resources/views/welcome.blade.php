e1072c104bbd84789307b639a91fb2c25d536c89
Lenin Lima <lejalima@espol.edu.ec>
2018-09-08 11:54:07 -0500

    mejora del welcome final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,9 +68,9 @@
                     <div class="col-sm-12"></div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-2"></div>
+                    <div class="col-sm-1"></div>
                     <div class="col-sm-10">
-                        <svg id='barGraph' width = "700" height = "400"></svg>
+                        <svg id='barGraph' width = "1000" height = "400"></svg>
                         <script> 
                             var margin = {top: 20, right: 20, bottom: 70, left: 40},
                                 width = 600 - margin.left - margin.right,
